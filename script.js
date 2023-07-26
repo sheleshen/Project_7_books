726549aef1322f72a79433103dddf10e5588cc68
sheleshen <sheleshnewaanna@gmail.com>
2023-07-26 20:40:37 +0300

    js | edit book

--- script.js
+++ script.js
@@ -69,6 +69,9 @@ function openModalWindow() {
 function closeModalWindow() {  
     const addBook = document.getElementById('modal-window')
     addBook.style.display = "none"
+
+    clearForm()
+    styleСhange()
 }
 
 //Очистить форму
@@ -79,19 +82,43 @@ function clearForm() {
     document.getElementById('bookImage').value = ""
 }
 
+//Смена стиля для input
+function styleСhange() {
+    document.getElementById('bookName').classList.toggle('input-error')
+}
+
 //Добавляем данные и очищаем форму
 function addBook() {
-    //скрыть форму при клике на кннопку "Сохранить"
-    const addBook = document.getElementById('modal-window')
-    addBook.style.display = "none" //скрыли форму
-
     //добавить книгу в список
     const nameValue = document.getElementById('bookName').value
     const authorValue = document.getElementById('bookAuthor').value
     const yearValue = document.getElementById('bookYear').value
     const imageValue = document.getElementById('bookImage').value
 
-    //данные новой книги
+    //Проверка для обязательных полей
+    if (nameValue === ''){
+        document.getElementById('bookName').classList.toggle('input-error')
+        // document.getElementById('bookNameError').innerHTML = 'Заполните поле'
+        return
+    }
+    btnSaveBook.addEventListener('click', styleСhange)
+
+    if (authorValue === ''){
+        document.getElementById('bookAuthor').classList.toggle('input-error')
+        return
+    }
+
+    if (yearValue === ''){
+        document.getElementById('bookYear').classList.toggle('input-error')
+        return
+    }
+
+    if (imageValue === ''){
+        document.getElementById('bookImage').classList.toggle('input-error')
+        return
+    }
+
+    //данные новой книги - все значения value хранятся в этой переменной
     const newBook = {
         id: idCounterBook++,
         title: nameValue,
@@ -100,39 +127,89 @@ function addBook() {
         image: imageValue
     }
 
+    //скрыть форму при клике на кннопку "Сохранить"
+    const addBook = document.getElementById('modal-window')
+    addBook.style.display = "none" //скрыли форму
+
     books.push(newBook)
     renderBooks()
+    styleСhange()
     clearForm()
+    closeModalWindow()
     saveToLocalStorage()
     // console.log(books)
 }
 
 
-
 // мод.окно "Редактировать книгу"
-const ModalWindowEdit = document.getElementById('modal-window-edit') // Найти элемент "Редактировать книгу"
+const modalWindowEdit = document.getElementById('modal-window-edit') // Найти элемент "Редактировать книгу"
 
-// const btnEditBook = document.getElementById('btn-open-modal-window-edit') // Кнопка вызов формы "Редактировать книгу"
-// btnEditBook.addEventListener('click', openModalWindowEdit) 
+const btnCloseModalWindowEdit = document.getElementById('btn-close-modal-window-edit')   // Кнопка "Закрыть" мод.окно "Ред. книгу"
+btnCloseModalWindowEdit.addEventListener('click', closeModalWindowEdit)
 
+const iconCloseModalWindowEdit = document.getElementById('icon-close-modal-window-edit') // Иконка "Закрыть" мод.окно "Ред. книгу"
+iconCloseModalWindowEdit.addEventListener('click', closeModalWindowEdit)
+
+const btnSaveEdit = document.getElementById('btn-save-edit') // Кнопка "Сохранить книгу" мод.окно "Ред. книгу"
 
 
 // ФУНКЦИИ мод.окно "Изменить книгу"
 
+// Получить данные редактируемой книги  
+function getInfoBook(book) {
+    document.getElementById('bookNameEdit').value = book.title
+    document.getElementById('bookAuthorEdit').value = book.authors
+    document.getElementById('bookYearEdit').value = book.year
+    document.getElementById('bookImageEdit').value = book.image
+}
+
 //Открыть форму редактирования при клике
 function openModalWindowEdit(id) {
-    const editbook = document.getElementById('modal-window-edit')
-    editbook.style.display = "flex"
+    modalWindowEdit.style.display = "flex"
 
     //найти книгу по её id
-    const bookEdit = books.find((n) => {
+    const book = books.find((n) => {
         return n.id === id 
     })
 
-    // получить данные по id
+    // получить данные полей по id
     getInfoBook(book);
+
+    const makeEdit = () => saveEditBook(book.id, makeEdit);
+    btnSaveEdit.addEventListener("click", makeEdit);
     
+    // saveToLocalStorage()
 }
+
+function saveEditBook(id, makeEdit) {
+    // функция редактировать книгу по нажатию кнопки
+    let book = books.find((findBook) => {
+      //ищу нужную книгу
+      return findBook.id === id; //найти книгу по id
+    });
+  
+    const bookIndexUp = books.indexOf(book);    //присвоить переменной индексы книг из массива
+  
+    const nameEdit = document.getElementById("bookNameEdit").value;
+    const authorEdit = document.getElementById("bookAuthorEdit").value;
+    const yearEdit = document.getElementById("bookYearEdit").value;
+    const imageEdit = document.getElementById("bookImageEdit").value;
+  
+    const newBookEdit = {
+      id,
+      title: nameEdit,
+      authors: authorEdit,
+      year: yearEdit,
+      image: imageEdit,
+    };
+  
+    btnSaveEdit.removeEventListener("click", makeEdit);  //удалить лишних обработчиков
+    books.splice(bookIndexUp, 1, newBookEdit); //удалять книгу и вставлять новую
+    renderBooks();
+    saveToLocalStorage();
+    closeModalWindowEdit();
+  }
+  
 
 //Закрыть форму редактирования при клике
 function closeModalWindowEdit() {  
@@ -140,19 +217,8 @@ function closeModalWindowEdit() {
     editBook.style.display = "none"
 }
 
-// Получить данные редактируемой книги  
-function getInfoBook(book) {
-    document.getElementById('bookName').value = book.title
-    document.getElementById('bookAuthor').value = book.authors
-    document.getElementById('bookYear').value = book.year
-    document.getElementById('bookImage').value = book.image
-}
 
-
-// УДАЛЕНИЕ КНИГИ ТЕСТ
-
-// const btnDeleteBook  = document.getElementById('btn-delete-book-${book.id}')
-// btnDeleteBook.addEventListener('click', deleteBook)
+// УДАЛЕНИЕ КНИГИ
 
 //Удаление данных книги ЧЕРЕЗ onclick
 function deleteBook(id) {
@@ -164,7 +230,7 @@ function deleteBook(id) {
     //индекс книги в массиве
     const bookIndex = books.indexOf(bookDelete)
 
-    //удалить элемент из массива
+    //удалить элемент из массива (1 шт)
     books.splice(bookIndex, 1)
 
     //показать новый список книг
@@ -200,13 +266,29 @@ function renderBooks() {
                     <button id="btn-open-modal-window-edit-${book.id}" class="btn small">
                         Изменить
                     </button>
-                    <button class="btn-sec small" onclick='deleteBook(${book.id})'> <!-- id="btn-delete-book-${book.id}"  -->
+                    <button class="btn-sec small" id="btn-delete-book-${book.id}"> <!-- onclick='deleteBook(${book.id})'  -->
                         Удалить
                     </button>
                 </div>
             </div>
         `
     })
+
+    //проходимся по каждому элементу массива
+    books.forEach((book) => {
+        const btnDeleteBook  = document.getElementById(`btn-delete-book-${book.id}`)
+        btnDeleteBook.addEventListener('click', () => {
+            deleteBook(book.id)      //здесь добавляем id, иначе все книги удалятся
+        });
+    });
+
+    // проходимся по каждому элементу массива
+    books.forEach((book) => {
+        const btnOpenModalEdit  = document.getElementById(`btn-open-modal-window-edit-${book.id}`)
+        btnOpenModalEdit.addEventListener('click', () => {
+            openModalWindowEdit(book.id)      //здесь добавляем id, иначе не понятно, что редактировать
+        });
+    });   
 
     saveToLocalStorage()
 }
